b8d701573c9c7facf8679a990300fc01edab049d
Yogesh Pande <Yogesh.Pande@arm.com>
2017-02-22 17:30:40 +0200

    Fixing issues with unit tests and other PR check fails

--- test/mbed-client-mbed-tls/unittest/stub/nsdlaccesshelper_stub.h
+++ test/mbed-client-mbed-tls/unittest/stub/nsdlaccesshelper_stub.h
@@ -13,34 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef COMMON_STUB_H
-#define COMMON_STUB_H
+#ifndef NSDLACCESSHELPER_STUB_H
+#define NSDLACCESSHELPER_STUB_H
 
-#include <sys/types.h>
-#include <netdb.h>
-#include "sockets/UDPSocket.h"
 #include "nsdlaccesshelper.h"
-#include "ip6string.h"
-#include "Ticker.h"
 
 //some internal test related stuff
-namespace common_stub
+namespace nsdlaccesshelper_stub
 {
-    extern socket_error_t error;
-    extern socket_event_t* event;
-    extern socket_addr* addr;
-    extern bool visited;
     extern bool bool_value;
-    extern void* void_value;
-    extern int int_value;
-    extern int int2_value;
-    extern uint16_t uint_value;
-    extern addrinfo* addrinfo;
-    extern sn_coap_hdr_s *coap_header;
-    extern sn_nsdl_dynamic_resource_parameters_s *resource;
-    extern pthread_t thread;
-    extern const char *char_value;
+    extern void *void_value;
+    extern uint8_t int_value;
     void clear();
 }
-
-#endif // COMMON_STUB_H
+#endif // NSDLACCESSHELPER_STUB_H
